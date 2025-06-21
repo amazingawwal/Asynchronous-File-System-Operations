2a0cfc8a5cc9f69fece4252872b6c6deb9328649
amazingawwal <sakeebmawwal@gmail.com>
2025-06-21 19:00:34 +0100

    Task 2 done

--- fs-op.js
+++ fs-op.js
@@ -25,4 +25,23 @@ readFileAsync('./test-files/sample.txt');
 //                                       }
 
 
-// Task 2: Read and Display File Contents with Try....Catch
+// Task 2: Write Content to File with Async/Await
+
+const fsw = require('fs/promises');
+
+const content = 'This is my task submission'; 
+const filename = './test-files/output.txt';
+
+async function writeFileAsync (filename, content){
+    try {
+        await fsw.writeFile(filename, content);
+        console.log('File written successfully!');
+    } catch (err) {
+        console.log('Error writing file', err);
+    }
+};
+
+writeFileAsync(filename, content);
+
+
+// Task 3: Copy File from Source to Destination
