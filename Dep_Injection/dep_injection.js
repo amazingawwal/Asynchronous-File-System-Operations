9edba7c9474649566bca9333c5c7c7ca2f6de080
amazingawwal <sakeebmawwal@gmail.com>
2025-06-22 18:35:25 +0100

    Task 1 and 2 using DI

--- Dep_Injection/dep_injection.js
+++ Dep_Injection/dep_injection.js
@@ -1,19 +1,30 @@
-class LoadFile {
-    constructor(filePath){
-        this.filePath = filePath
+const fs = require('fs/promises') 
+
+
+
+class LoadFilename {
+    constructor(filePath, content){
+        this.filePath = filePath,
+        this.content = content
     };
 
-    setFilePath(){
-        return this.filePath
-    };
+    // setFilePath(){
+    //     return this.filePath
+    // };
+
+    // setContent(){
+    //     return this.content
+    // };
 };
 
+
+// Read File Service
 class readFile {
      constructor(filePath){
         this.filePath = filePath
     };
 
-    async readFileAsync (){
+    async readFile (){
         try {
             const data = await fs.readFile(this.filePath, 'utf-8');
             console.log(data)
@@ -23,8 +34,33 @@ class readFile {
     };
 };
 
-const filePath = new LoadFile('./test-files/sample.txt').setFilePath()
-const readText = new readFile(filePath);
-
+const filePath = new LoadFilename('../test-files/sample.txt');
+const readText = new readFile(filePath.filePath);
 // console.log(filePath)
-readText.readFileAsync();
+readText.readFile();
+
+
+//Write File Service
+
+const content = 'This is my task submission'; 
+const filename = '../Dep_Injection/output.txt';
+
+class WriteFile {
+    constructor({filePath, content}){
+        this.filePath = filePath,
+        this.content = content
+    };
+
+    async writeFile (){
+    try {
+        await fs.writeFile(this.filePath, this.content);
+        console.log(`Content: ${this.content} written successfully to ${this.filePath}!`);
+    } catch (err) {
+        console.log('Error writing file', err);
+    }
+  }
+};
+
+const path = new LoadFilename(filename, content);
+const writeText = new WriteFile(path);
+writeText.writeFile();
